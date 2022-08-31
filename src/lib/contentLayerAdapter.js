cb9301ef99b389ed93ed7290a32a77bbedb989eb
Eason Chang <eason@easonchang.com>
2022-09-01 00:22:10 +0800

    Show markdown posts in IndexPage

--- src/lib/contentLayerAdapter.js
+++ src/lib/contentLayerAdapter.js
@@ -4,6 +4,11 @@ import {
   defineNestedType,
   makeSource,
 } from 'contentlayer/source-files';
+import { compareDesc } from 'date-fns';
 
 export { allPosts, defineDocumentType, defineNestedType, makeSource, Post };
 
+export const allPostsNewToOld =
+  allPosts?.sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date));
+  }) || [];
