4a4358fbeaa5ee3ec4941085d990f7380072aa39
Eason Chang <eason@easonchang.com>
2022-09-02 23:20:58 +0800

    Install next-themes for dark mode
    Refs:
    https://github.com/pacocoursey/next-themes#with-tailwind
    https://tailwindcss.com/docs/dark-mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
