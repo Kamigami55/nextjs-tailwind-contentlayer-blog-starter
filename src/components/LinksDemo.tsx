b4a79a42b201920e1fcead17b201fec3ac760360
Eason Chang <eason@easonchang.com>
2022-09-06 04:04:27 +0800

    Add post to demo custom link

---
+++ src/components/LinksDemo.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+
+export default function LinksDemo() {
+  return (
+    <div>
+      <p>
+        <a href="/">Go to homepage</a>
+      </p>
+      <p>
+        <a href="https://google.com">Go to Google</a>
+      </p>
+      <h3 id="demo-title">Demo title</h3>
+      <p>
+        <a href="#demo-title">Go to demo title anchor</a>
+      </p>
+    </div>
+  );
+}
